1d363d60003343a2a6de4cff341b0b8fc4ebaf75
Pixel Prowler <68132166+LiuWoodsCode@users.noreply.github.com>
2026-08-25 15:13:27 -0400

    Start work on native Ranboo client in swiftui

---
+++ Code/Ranboo/RanbooSwiftUI/RanbooSwiftUI/RanbooSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RanbooSwiftUIApp.swift
+//  RanbooSwiftUI
+//
+//  Created by Pixel Prowler on 8/25/26.
+//
+
+import SwiftUI
+
+@main
+struct RanbooSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
